ec18e89a9bb8b6debf68223a688c444cfd08d6fe
sherif-elmetainy <sherif.elmetainy@gmail.com>
2026-03-20 11:15:01 +0200

    Add person name with tests

---
+++ src/CodeArt.Poc.Primitives/PersonName.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+using JetBrains.Annotations;
+
+using Vogen;
+
+namespace CodeArt.Poc.Primitives;
+
+[ValueObject<string>]
+public readonly partial struct PersonName
+{
+    /// <summary>
+    /// Maximum length of person name
+    /// </summary>
+    public const int MaxLength = 60;
+
+    /// <summary>
+    /// Letter sub expression (includes letters in all languages)
+    /// </summary>
+    private const string LetterExpression = @"(?:\s|\p{Ll}|\p{Lu}|\p{Lt}|\p{Lo}|\p{Lm})";
+
+    /// <summary>
+    /// Person's name regular expression
+    /// </summary>
+    [UsedImplicitly]
+    public const string RegularExpression =
+        $@"^{LetterExpression}+(?:\-|\'|{LetterExpression})*$";
+
+    private static string NormalizeInput(string input)
+    {
+        return input.Trim();
+    }
+
+    private static Validation Validate(string input)
+    {
+        switch (input)
+        {
+            case { Length : 0 }:
+                return Validation.Invalid(ValidationErrors.PersonNameCannotBeEmpty);
+            case { Length : > MaxLength }:
+                return Validation.Invalid(string.Format(ValidationErrors.PersonNameTooLong, MaxLength));
+            default:
+                {
+                    var isValid = Regex.IsMatch(input);
+                    return isValid ? Validation.Ok : Validation.Invalid(ValidationErrors.PersonNameInvalid);
+                }
+        }
+    }
+
+    [UsedImplicitly]
+    public static readonly Regex Regex = GetPersonNameRegex();
+
+    [GeneratedRegex(RegularExpression, RegexOptions.Compiled | RegexOptions.CultureInvariant)]
+    private static partial Regex GetPersonNameRegex();
+}
---
+++ tests/CodeArt.Poc.Primitives.Tests/PersonNameTest.cs
@@ -0,0 +1,87 @@
+﻿namespace CodeArt.Poc.Primitives.Tests;
+
+public class PersonNameTest
+{
+    [Fact]
+    public void Should_Accept_English_Names()
+    {
+        var result = PersonName.TryFrom("John");
+        Assert.True(result.IsSuccess);
+    }
+    
+    [Fact]
+    public void Should_Accept_English_Name_With_Space()
+    {
+        var result = PersonName.TryFrom("John Doe");
+        Assert.True(result.IsSuccess);
+    }
+    
+    [Fact]
+    public void Should_Accept_English_Name_With_SingleQuote()
+    {
+        var result = PersonName.TryFrom("O'Connor");
+        Assert.True(result.IsSuccess);
+    }
+    
+    [Fact]
+    public void Should_Accept_Arabic_Name()
+    {
+        var result = PersonName.TryFrom("شريف");
+        Assert.True(result.IsSuccess);
+    }
+    
+    [Fact]
+    public void Should_Accept_Russian_Name()
+    {
+        var result = PersonName.TryFrom("Владимир");
+        Assert.True(result.IsSuccess);
+    }
+    
+    [Fact]
+    public void Should_Trim_Spaces()
+    {
+        var result = PersonName.TryFrom(" John Doe  ");
+        Assert.True(result.IsSuccess);
+        Assert.Equal("John Doe", result.ValueObject.Value);
+    }
+    
+    [Fact]
+    public void Should_Reject_Empty()
+    {
+        var result = PersonName.TryFrom("");
+        Assert.False(result.IsSuccess);
+        Assert.Equal(result.Error.ErrorMessage, ValidationErrors.PersonNameCannotBeEmpty);
+    }
+    
+    [Fact]
+    public void Should_Reject_Whitespace()
+    {
+        var result = PersonName.TryFrom(" ");
+        Assert.False(result.IsSuccess);
+        Assert.Equal(result.Error.ErrorMessage, ValidationErrors.PersonNameCannotBeEmpty);
+    }
+    
+    [Fact]
+    public void Should_Reject_TooLong()
+    {
+        var result = PersonName.TryFrom(new string('a', PersonName.MaxLength + 1));
+        Assert.False(result.IsSuccess);
+        Assert.Equal(result.Error.ErrorMessage, string.Format(ValidationErrors.PersonNameTooLong, PersonName.MaxLength));
+    }
+    
+    [Fact]
+    public void Should_Reject_Invalid()
+    {
+        var result = PersonName.TryFrom("?");
+        Assert.False(result.IsSuccess);
+        Assert.Equal(result.Error.ErrorMessage, ValidationErrors.PersonNameInvalid);
+    }
+    
+    [Fact]
+    public void Should_Reject_Numbers()
+    {
+        var result = PersonName.TryFrom("a1");
+        Assert.False(result.IsSuccess);
+        Assert.Equal(result.Error.ErrorMessage, ValidationErrors.PersonNameInvalid);
+    }
+}
